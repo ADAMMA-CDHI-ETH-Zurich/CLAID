8c8d5aa9fbcc3b9766642ef84202d2da0dcfff96
Patrick Langer <planger@guests-als-1-155.intern.ethz.ch>
2022-10-18 15:35:48 +0200

    Added CLAID_LINK_LIBRARIES to cmake utils.

--- CMakeUtils/CLAIDConfig.cmake
+++ CMakeUtils/CLAIDConfig.cmake
@@ -1,3 +1,18 @@
 message("FoundCLAID")
 set (CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${PROJECT_SOURCE_DIR}/cmake/Modules/")
 include(${CMAKE_CURRENT_LIST_DIR}/CLAID_Include.cmake)
+
+if(UNIX AND NOT APPLE)
+    # for Linux, BSD, Solaris, Minix
+    set(CLAID_LINK_LIBRARIES pthread)
+endif()
+
+if(APPLE)
+    # for MacOS X or iOS, watchOS, tvOS (since 3.10.3)
+    set(CLAID_LINK_LIBRARIES pthread)
+endif()
+
+if(WIN32)
+    # for Windows operating system in general
+    set(CLAID_LINK_LIBRARIES wsock32 ws2_32)
+endif()
