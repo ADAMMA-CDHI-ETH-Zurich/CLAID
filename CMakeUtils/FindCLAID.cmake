7a725552b242c5a5c5093bdc84f718cfdda70212
Patrick Langer <planger@guests-als-1-155.intern.ethz.ch>
2022-10-25 19:34:42 +0200

    Changed CMakeUtils to work on windows, linux and mac by using platform specific path separator (/ vs \\)

--- CMakeUtils/FindCLAID.cmake
+++ CMakeUtils/FindCLAID.cmake
@@ -3,5 +3,19 @@
 # cross compiling environments such as in android studio). In that case, this path needs
 # to be added to CMAKE_MODULE_PATH manually.
 message("FoundCLAID")
-set (CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${PROJECT_SOURCE_DIR}/cmake/Modules/")
-include(${CMAKE_CURRENT_LIST_DIR}/CLAID_Include.cmake)
+
+
+if(UNIX)
+    # for Linux, BSD, Solaris, Minix, Apple
+    set (CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${PROJECT_SOURCE_DIR}/cmake/Modules/")
+    include(${CMAKE_CURRENT_LIST_DIR}${OS_SLASH}CLAID_Include.cmake)
+endif()
+
+if(WIN32)
+    # for Windows operating system in general
+    set (CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${PROJECT_SOURCE_DIR}\\cmake\\Modules\\")
+    include(${CMAKE_CURRENT_LIST_DIR}${OS_SLASH}CLAID_Include.cmake)
+endif()
+
+
+
