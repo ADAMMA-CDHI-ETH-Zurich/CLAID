2d97189ff1973aa279e091c2f50cc7903bed1597
Patrick Langer <planger@guests-als-1-155.intern.ethz.ch>
2022-12-06 16:40:45 +0100

    Added FileUtils::removeDirectoryRecursively.

--- include/Utilities/FileUtils.hpp
+++ include/Utilities/FileUtils.hpp
@@ -15,10 +15,14 @@ namespace claid
 
             static bool createDirectoriesRecursively(std::string path);
             static bool createDirectory(std::string path);
+            static bool removeDirectoryRecursively(const std::string& path);
 
             static bool getAllDirectoriesInDirectory(std::string path, std::vector<std::string>& output);
 
             static bool readFileToString(std::string path, std::string& content);
+
+            static bool removeFile(const std::string& path);
+            static bool removeFileIfExists(const std::string& path);
 
     };
 }
