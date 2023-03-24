a1ef9229a37c338e920f7fc2b5b12cf3375f1ad6
Patrick Langer <planger@guests-als-1-155.intern.ethz.ch>
2023-03-24 10:54:57 +0100

    Added Path utility.

--- include/Serialization/AbstractSerializer.hpp
+++ include/Serialization/AbstractSerializer.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
-
+#include "TypeChecking/TypeCheckingFunctions.hpp"
+#include "Exception/Exception.hpp"
 namespace claid
 {
     class AbstractSerializer
@@ -12,6 +13,11 @@ namespace claid
 
             }
 
-            virtual bool getByteRepresentationOfSerializedData(std::vector<char>& data) = 0;
+            virtual void getDataWriteableToFile(std::vector<char>& data)
+            {
+                CLAID_THROW(Exception, "Data was serialized using " << this->getReflectorName() << ", which shall now be written to a file.\n"
+                << "However, Serializer " << this->getReflectorName() << " does not support to transform serialized data to a representation that can be written to a file.");
+            }
+            
     };
 }
---
+++ include/Utilities/Path.hpp
@@ -0,0 +1,120 @@
+#pragma once
+
+#include <string>
+#include "Utilities/Time.hpp"
+
+
+namespace claid
+{
+    class Path
+    {
+        private:
+            std::string path;
+
+            size_t getIndexOfRightMostPathSeparator()
+            {
+                if(path.size() == 0)
+                {
+                    return 0;
+                }
+
+                size_t index = path.size() - 1;
+
+                while(index > 0)
+                {
+                    if(this->path[index] == '/' || this->path[index] == '\\')
+                    {
+                        return index;
+                    }
+
+                    index--;
+                }
+
+                return 0;
+            }
+
+        public:
+
+            Path()
+            {
+
+            }
+
+            Path(std::string path) : path(path)
+            {
+
+            }
+
+            std::string getFolderPath()
+            {
+                size_t index = this->getIndexOfRightMostPathSeparator();
+                if(index == 0)
+                {
+                    return "";
+                }
+
+                return this->path.substr(0, index);
+            }
+
+            std::string getFilePath()
+            {
+                size_t index = this->getIndexOfRightMostPathSeparator();
+                if(index == 0)
+                {
+                    // No path separator found, assume the path points to a file.
+                    return this->path;
+                }
+                return "";
+            }
+
+            std::string getFileSuffix()
+            {
+                if(path.size() == 0)
+                {
+                    return "";
+                }
+
+                size_t index = path.size() - 1;
+            
+                while(index > 0)
+                {
+                    if(this->path[index] == '.')
+                    {
+                        return this->path.substr(index + 1, this->path.size());
+                    }
+
+                    index--;
+                }
+
+                return "";
+            }
+
+            std::string toString() const
+            {
+                return this->path;
+            }
+
+            operator std::string() const
+            {
+                return this->path;
+            }
+
+            bool operator==(const Path& rhs) const
+            {
+                return this->toString() == rhs.toString();
+            }
+
+            bool operator!=(const Path& rhs) const
+            {
+                return this->toString() != rhs.toString();
+            }
+
+            Path& operator=(const std::string& path)
+            {
+                this->path = path;
+                return *this;
+            }
+
+
+    };
+}
