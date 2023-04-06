9c59bc8a23923e321223aa6a2e939f802162e734
Patrick <planger@ethz.ch>
2023-04-06 10:53:06 +0200

    Renamed environment variable.

--- install_windows.bat
+++ install_windows.bat
@@ -4,7 +4,7 @@ echo "Installing CLAID into %mypath:~0,-1%"
 setx CLAID_PATH "%mypath:~0,-1%"
 setx CLAID_CMAKE_MODULE_PATH "%mypath:~0,-1%"\CMakeUtils\
 setx CMAKE_PREFIX_PATH "%mypath:~0,-1%"\CMakeUtils\
-setx "python C:\Users\planger\Desktop\CLAID\claid_manager\claid"
+setx CLAID "python C:\Users\planger\Desktop\CLAID\claid_manager\claid"
 
 
 echo "CLAID installed successfully."
