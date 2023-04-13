9f2235f82150b935ada1b89bcf592693f3f6c3db
Patrick <patrick.langer97@gmx.de>
2023-04-13 20:43:50 +0200

    Fixed hardcoded path.

--- install_windows.bat
+++ install_windows.bat
@@ -3,8 +3,8 @@ SET mypath=%~dp0
 echo "Installing CLAID into %mypath:~0,-1%"
 setx CLAID_PATH "%mypath:~0,-1%"
 setx CLAID_CMAKE_MODULE_PATH "%mypath:~0,-1%"\CMakeUtils\
-setx CMAKE_PREFIX_PATH "%mypath:~0,-1%"\CMakeUtils\
-setx CLAID "python C:\Users\planger\Desktop\CLAID\claid_manager\claid"
+setx CMAKE_PREFIX_PATH "%CMAKE_PREFIX_PATH%;%mypath:~0,-1%"\CMakeUtils\
+setx claid "python %mypath:~0,-1%\claid_manager\claid"
 
 
 echo "CLAID installed successfully."
