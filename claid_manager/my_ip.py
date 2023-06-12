ee09bdce8a46aa4ae7b6b856a12fe7aedccbd791
Patrick Langer <planger@guests-als-1-155.intern.ethz.ch>
2023-06-12 12:52:16 +0200

    Used presumably better method to detect local IP address.

--- claid_manager/my_ip.py
+++ claid_manager/my_ip.py
@@ -1,8 +1,19 @@
 import socket  
+def get_ip_address():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    return s.getsockname()[0]
 
 def my_ip(*args):
 
+    print("Trying to query IP Address.")
+    try:
+        ip_address = get_ip_address()
+        print("Found IP address of network facing interface:", ip_address)
+    except:
+        print("Failed to look up local IP address for host. Try using \"ifconfig\" or \"ipconfig\" instead.")
 
+    return
     hostname=socket.gethostname()   
     
     print("Tring to query IP for host {}".format(hostname))
