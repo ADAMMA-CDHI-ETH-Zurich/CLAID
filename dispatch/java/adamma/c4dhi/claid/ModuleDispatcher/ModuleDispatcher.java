d168b35d6992c73e50379aeb38c2453d704b1d2b
Patrick Langer <planger@guests-als-1-155.intern.ethz.ch>
2023-10-16 18:24:57 +0200

    Began implementing java support.

---
+++ dispatch/java/adamma/c4dhi/claid/ModuleDispatcher/ModuleDispatcher.java
@@ -0,0 +1,7 @@
+import adamma.c4dhi.claid.ClaidServiceStub;
+
+// Corresponds to ServiceImpl of the C++ core.
+public class ModuleDispatcher  
+{
+    // private ClaidService stub;
+}
---
+++ dispatch/java/adamma/c4dhi/claid/claid_java_test.java
@@ -0,0 +1,13 @@
+package adamma.c4dhi.claid;
+
+public class claid_java_test {
+
+	static {
+		System.loadLibrary("claid_capi");
+	}
+	public static void main(String[] args){
+
+		System.out.println("Hello, World!");
+	
+	}
+}
