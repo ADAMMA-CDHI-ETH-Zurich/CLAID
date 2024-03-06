78324807ea1a12c0f461937f57999f5ed89940c6
Patrick Langer <planger@guests-als-1-155.intern.ethz.ch>
2024-03-06 13:38:07 +0100

    Added README.

--- dispatch/python/setup.py
+++ dispatch/python/setup.py
@@ -1,15 +1,20 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='claid',
-    version='0.0.1',    
-    description='A example Python package',
-    url='https://github.com/shuds13/pyexample',
+    version='0.0.2',    
+    description='CLAID Pyathon package (PyCLAID)',
+    url='https://claid.ch',
     author='Patrick Langer',
-    author_email='patrick.langer97@gmx.de',
+    author_email='planger@ethz.ch',
     license='Apache 2',
     packages=find_packages(),
     include_package_data=True,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=['protobuf',
                       'grpcio',
                       'numpy',      
@@ -18,16 +23,23 @@ setup(
                       ],
 
     classifiers=[
-        'Development Status :: 1 - Planning',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',  
+        'Intended Audience :: Developers',
+        'Intended Audience :: Healthcare Industry',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: Apache Software License',  
+        'Operating System :: Android',
+        'Operating System :: MacOS',
         'Operating System :: POSIX :: Linux',        
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: Unix',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
+    python_requires='>=3.7',
 
     entry_points='''
         [console_scripts]
